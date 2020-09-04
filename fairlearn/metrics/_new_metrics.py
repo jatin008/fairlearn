bc8abd7d567c38a787631f7cf8d177094f11d688
Richard Edgar <riedgar@microsoft.com>
2020-09-04 14:28:20 -0400

    Fiddling around... things seem promising
    Signed-off-by: Richard Edgar <riedgar@microsoft.com>

--- fairlearn/metrics/_new_metrics.py
+++ fairlearn/metrics/_new_metrics.py
@@ -1,6 +1,11 @@
 # Copyright (c) Microsoft Corporation and contributors.
 # Licensed under the MIT License.
 
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+
+
 class GroupedMetric:
     def __init__(self):
         self._overall = None
@@ -16,4 +21,30 @@ class GroupedMetric:
 
 
 def group_summary(metric_function, y_true, y_pred, sensitive_features):
-    pass
+    result = GroupedMetric()
+    result._overall = metric_function(y_true, y_pred)
+
+    _yt = np.asarray(y_true)
+    _yp = np.asarray(y_pred)
+
+    sf_names = []
+    sf_classes = []
+    sf_encoded = []
+    for s_f in sensitive_features:
+        sf_names.append(s_f.name)
+        le = LabelEncoder()
+        sf_encoded.append(le.fit_transform(s_f))
+        sf_classes.append(le.classes_)
+
+    idx = pd.MultiIndex.from_product(sf_classes, names=sf_names)
+
+    result._by_groups = pd.Series(None, index=idx)
+    for current_groups in idx:
+        mask = np.ones(len(y_true))
+        for i in range(len(current_groups)):
+            nxt_mask = (np.asarray(sensitive_features[i]) == current_groups[i])
+            mask = (mask == nxt_mask)
+
+        result._by_groups[current_groups] = metric_function(_yt[mask], _yp[mask])
+
+    return result
--- test/unit/metrics/test_new_metrics.py
+++ test/unit/metrics/test_new_metrics.py
@@ -1,19 +1,51 @@
 # Copyright (c) Microsoft Corporation and contributors.
 # Licensed under the MIT License.
 
+import numpy as np
+import pandas as pd
+import sklearn.metrics as skm
+
+from fairlearn.metrics._new_metrics import group_summary
+
+from fairlearn.metrics import accuracy_score_group_summary
+from fairlearn._input_validation import _compress_multiple_sensitive_features_into_single_column
+
 y_true = [0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 1,
-          0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0]
+          0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0]
 y_pred = [1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1,
           0, 0, 0, 1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 1, 1]
-sf_1 = ['a', 'b', 'a', 'b', 'a', 'b', 'a', 'b', 'a', 'b', 'a', 'b', 'a', 'b', 'a', 'b',
-        'a', 'b', 'a', 'b', 'a', 'b', 'a', 'b', 'a', 'b', 'a', 'b', 'a', 'b', 'a', 'b',
-        'a', 'b', 'a', 'b']
-sf_2 = ['A', 'B', 'C', 'A', 'B', 'C', 'A', 'B', 'C', 'A', 'B', 'C', 'A', 'B', 'C', 'A',
-        'B', 'C', 'A', 'B', 'C', 'A', 'B', 'C', 'A', 'B', 'C', 'A', 'B', 'C', 'A', 'B',
-        'C', 'A', 'B', 'C']
+sf_1 = pd.Series(['a', 'b', 'a', 'b', 'a', 'b', 'a', 'b', 'a', 'b', 'a', 'b', 'a', 'b', 'a', 'b',
+                  'a', 'b', 'a', 'b', 'a', 'b', 'a', 'b', 'a', 'b', 'a', 'b', 'a', 'b', 'a', 'b',
+                  'a', 'b', 'a', 'b'], name="SF 1")
+sf_2 = pd.Series(['A', 'B', 'C', 'A', 'B', 'C', 'A', 'B', 'C', 'A', 'B', 'C', 'A', 'B', 'C', 'A',
+                  'B', 'C', 'A', 'B', 'C', 'A', 'B', 'C', 'A', 'B', 'C', 'A', 'B', 'C', 'A', 'B',
+                  'C', 'A', 'B', 'C'], name="SF 2")
 
 
 def test_data_lengths():
     assert len(y_true) == len(y_pred)
     assert len(y_true) == len(sf_1)
     assert len(y_true) == len(sf_2)
+
+
+def test_learning():
+    result = group_summary(skm.accuracy_score, y_true, y_pred, [sf_1, sf_2])
+    both_sf = np.stack(sf_1.to_numpy(), sf_2.to_numpy())
+    sf_combined = _compress_multiple_sensitive_features_into_single_column(both_sf)
+    print(sf_combined)
+    expected = accuracy_score_group_summary(y_true, y_pred, sensitive_features=sf_combined)
+
+    assert result.overall == skm.accuracy_score(y_true, y_pred)
+    assert len(result.by_groups) == 6
+    print("expected =", expected)
+    print("Acutal\n", result.by_groups)
+    assert False
+
+
+def test_learning2():
+    result = group_summary(skm.accuracy_score, y_true, y_pred, [sf_1])
+    expected = accuracy_score_group_summary(y_true, y_pred, sensitive_features=sf_1)
+
+    print("expected =", expected)
+    print("Acutal", result.by_groups)
+    assert False
