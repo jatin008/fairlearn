a88dbf9ca12098be5973b0c6a01f56dd79a0533e
Richard Edgar <riedgar@microsoft.com>
2020-09-08 09:25:14 -0400

    Parameterise over a few options
    Signed-off-by: Richard Edgar <riedgar@microsoft.com>

--- test/unit/reductions/exponentiated_gradient/test_exponentiatedgradient_pickle.py
+++ test/unit/reductions/exponentiated_gradient/test_exponentiatedgradient_pickle.py
@@ -1,17 +1,25 @@
 # Copyright (c) Microsoft Corporation and contributors.
 # Licensed under the MIT License.
 
+import joblib
+import pytest
+from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
+
 from fairlearn.reductions import DemographicParity, EqualizedOdds, ExponentiatedGradient, \
     TruePositiveRateParity, FalsePositiveRateParity
 
-import joblib
 from .test_utilities import _get_data
 
+_moments = [DemographicParity, EqualizedOdds, FalsePositiveRateParity, TruePositiveRateParity]
+_learners = [DecisionTreeClassifier, LogisticRegression]
 
-def test_pickle_smoke():
+
+@pytest.mark.parametrize("learner", _learners)
+@pytest.mark.parametrize("moment", _moments)
+def test_pickle_smoke(learner, moment):
     X, y, A = _get_data(A_two_dim=False, flip_y=False)
-    clf = ExponentiatedGradient(DecisionTreeClassifier(), TruePositiveRateParity())
+    clf = ExponentiatedGradient(learner(), moment())
     clf.fit(X, y, sensitive_features=A)
 
     joblib.dump(clf, 'file.pkl')
